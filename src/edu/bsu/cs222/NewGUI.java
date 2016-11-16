836a51338663764c870b80c8309d4e00e5bdfee1
Alex Baker <abbaker@bsu.edu>
2016-11-15 22:19:48 -0500

    Started to create new GUI

---
+++ src/edu/bsu/cs222/NewGUI.java
@@ -0,0 +1,11 @@
+package edu.bsu.cs222;
+
+import javax.swing.*;
+
+/**
+ * Created by AlexB on 11/15/2016.
+ */
+public class NewGUI {
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+}
