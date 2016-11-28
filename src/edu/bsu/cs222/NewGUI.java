a2c79e5439f7ec13fedd5909b3b50a798e0987ee
Alex Baker <abbaker@bsu.edu>
2016-11-28 00:15:48 -0500

    Add Text Field

--- src/edu/bsu/cs222/NewGUI.java
+++ src/edu/bsu/cs222/NewGUI.java
@@ -1,7 +1,11 @@
 package edu.bsu.cs222;
 
+import javax.swing.*;
+
 /**
  * Created by AlexB on 11/27/2016.
  */
 public class NewGUI {
+    private JPanel panel1;
+    private JTextArea textArea1;
 }
