754db7e11fb20aded20bc51ff0900f464fa2f80c
Alex Baker <abbaker@bsu.edu>
2016-12-19 23:33:35 -0600

    Create Main

---
+++ src/edu/bsu/cs222/Main.java
@@ -0,0 +1,33 @@
+package edu.bsu.cs222;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(Main.class, (java.lang.String[])null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            StackPane page = (StackPane) FXMLLoader.load(Main.class.getResource("GUI.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("FXML is Simple");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
